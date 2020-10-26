541e69b31f3909c8dbe3a78d569728e90bf57e2c
Adam Bridge <bridge.adam@gmail.com>
2020-10-26 19:52:14 -0400

    create db, removed sudos

--- do-it.sh
+++ do-it.sh
@@ -1,7 +1,5 @@
-sudo docker create --name postgres-demo -e POSTGRES_PASSWORD=Welcome -p 5432:5432 postgres:11.5-alpine
-
-sudo docker start postgres-demo
-
+docker create --name postgres-demo -e POSTGRES_PASSWORD=Welcome -p 5432:5432 postgres:11.5-alpine
+docker start postgres-demo
 # docker stop postgres-demo
 
 # Connection Info:
@@ -9,15 +7,13 @@ sudo docker start postgres-demo
 #    Username: `postgres`
 #    Password: `Welcome`
 
-
 # Connect to PSQL prompt from docker:  
 # sudo docker exec -it postgres-demo psql -U postgres
 
-#    psql> create database conference_app;
+docker exec -it postgres-demo psql -c "create database conference_app" -U postgres
 
+docker cp ./database/postgresql/create_tables.sql postgres-demo:/create_tables.sql
+docker exec -it postgres-demo psql -d conference_app -f create_tables.sql -U postgres
 
-sudo docker cp ./database/postgresql/create_tables.sql postgres-demo:/create_tables.sql
-sudo docker exec -it postgres-demo psql -d conference_app -f create_tables.sql -U postgres
-
-sudo docker cp ./database/postgresql/insert_data.sql postgres-demo:/insert_data.sql
-sudo docker exec -it postgres-demo psql -d conference_app -f insert_data.sql -U postgres
+docker cp ./database/postgresql/insert_data.sql postgres-demo:/insert_data.sql
+docker exec -it postgres-demo psql -d conference_app -f insert_data.sql -U postgres
